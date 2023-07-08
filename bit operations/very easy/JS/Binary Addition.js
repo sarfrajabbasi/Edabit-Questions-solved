21cd3dddbe9aca7ea682b74d3ab6411a73a77ef2
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-07-08 10:01:03 +0530

    using bitwise operator

---
+++ bit operations/very easy/JS/Binary Addition.js
@@ -0,0 +1,17 @@
+function addBinary(a, b) {
+//  return Number(a+b).toString(2)
+var binary =( a+b >>> 0).toString(2)
+return binary
+	
+}
+
+// enjoy :) 
+console.log(addBinary(1,1), '10')
+console.log(addBinary(1,2), '11')
+console.log(addBinary(4,5),'1001')
+console.log(addBinary(8,20),'11100')
+console.log(addBinary(100,20),'1111000')
+console.log(addBinary(40,50),'1011010')
+console.log(addBinary(65,77),'10001110')
+console.log(addBinary(40,50),'1011010')
+console.log(addBinary(1,0),'1')
---
+++ bit operations/very easy/JS/Boolean to String Conversion.js
@@ -0,0 +1,6 @@
+function boolToString(flag) {
+	return String(flag)
+}
+
+console.log(boolToString(true), "true")
+console.log(boolToString(false), "false")
